6faeab7ce77847f6c55bd1734ba207b18f8298b3
shreya-tikkoo-maersk <shreya.tikkoo@lns.maersk.com>
2025-02-15 22:32:03 +0530

    removing unused code

--- src/main/java/com/example/promotion/engine/exceptions/PromotionNotApplicableException.java
+++
@@ -1,7 +0,0 @@
-package com.example.promotion.engine.exceptions;
-
-public class PromotionNotApplicableException extends RuntimeException {
-    public PromotionNotApplicableException(String message) {
-        super(message);
-    }
-}
